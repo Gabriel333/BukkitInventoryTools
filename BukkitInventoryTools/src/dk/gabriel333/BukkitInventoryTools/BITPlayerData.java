12506effd129851dd024fd1f15f2008aac710ce0
Christian.L.Jensen <Christian.L.Jensen@W-5QMTX4J.DSV.COM>
2011-10-12 20:13:49 +0200

    Revert "2.5.4 too many errors."
    This reverts commit f4a2054c05ca1661277760500824ae93f2dd7231.

--- BukkitInventoryTools/src/dk/gabriel333/BukkitInventoryTools/BITPlayerData.java
+++ BukkitInventoryTools/src/dk/gabriel333/BukkitInventoryTools/BITPlayerData.java
@@ -1,47 +1,28 @@
 package dk.gabriel333.BukkitInventoryTools;
 
-import java.util.HashMap;
-import java.util.UUID;
-
-import org.getspout.spoutapi.gui.GenericTextField;
-import org.getspout.spoutapi.gui.PopupScreen;
-
 public class BITPlayerData {
-	
-	private PopupScreen popupGetPincode;
-	private PopupScreen popupSetPincode;
-	private GenericTextField pincode;
-	private GenericTextField owner;
-	private GenericTextField closetimer;
-	private GenericTextField coOwners;
-	private GenericTextField useCost;
-	private GenericTextField connectedTo;
-	private GenericTextField shared;
 
-	public BITPlayerData(PopupScreen popupGetPincode,
-			PopupScreen popupSetPincode, GenericTextField pincode,
-			GenericTextField owner,
-			GenericTextField closetimer,
-			GenericTextField coOwners,
-			GenericTextField useCost,
-			GenericTextField connectedTo,
-			GenericTextField shared) {
-		this.popupGetPincode = popupGetPincode;
-		this.popupSetPincode = popupSetPincode;
-		this.pincode = pincode;
-		this.owner = owner;
-		this.closetimer = closetimer;
-		this.coOwners = coOwners;
-		this.useCost = useCost;
-		this.shared = shared;
-	}
+    private boolean registered;
+    private boolean authenticated;
 
-	public void setUserno(Integer userno) {
-		this.userno = userno;
-	}
+    public BITPlayerData(boolean registered, boolean authenticated) {
+        this.registered = registered;
+        this.authenticated = authenticated;
+    }
 
-	public void setPopupGetPincode(PopupScreen popupGetPincode) {
-		this.popupGetPincode = popupGetPincode;
-	}
+    public boolean isRegistered() {
+        return registered;
+    }
 
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    public void setRegistered(boolean newvalue) {
+        registered = newvalue;
+    }
+
+    public void setAuthenticated(boolean newvalue) {
+        authenticated = newvalue;
+    }
 }

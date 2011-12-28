bb6df73d4737eb81c74ce9cc2330a520b55c1739
Mike Howe <mike@mcsnetworks.com>
2011-12-27 22:27:00 -0600

    1st.

---
+++ BukkitInventoryTools/src/dk/gabriel333/BITBackpack/BITBackpackInventorySaveTask.java
@@ -0,0 +1,93 @@
+package dk.gabriel333.BITBackpack;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.getspout.spoutapi.SpoutManager;
+import dk.gabriel333.BukkitInventoryTools.BIT;
+import dk.gabriel333.Library.BITConfig;
+
+public class BITBackpackInventorySaveTask implements Runnable {
+
+	@SuppressWarnings("unused")
+	private BIT plugin;
+
+	public void SBInventorySaveTask(BIT plugin) {
+	 this.plugin = plugin;
+	}
+
+	public static void saveAll() {
+		Player[] players = Bukkit.getServer().getOnlinePlayers();
+		HashMap<String, ItemStack[]> invs = new HashMap<String, ItemStack[]>(
+				BITBackpack.inventories);
+		for (Player player : players) {
+			saveInventory(player, player.getWorld());
+			if (invs.containsKey(player.getName())) {
+				invs.remove(player.getName());
+			}
+		}
+	}
+
+	public static void saveInventory(Player player, World world) {
+		
+		//TODO: Insert SAVE_TO_SQL. BITInventory.saveBitInventory(SpoutPlayer sPlayer, SpoutBlock block,
+		//String owner, String name, String coowners, Inventory inventory,
+		//int useCost);
+		
+		//BITInventory.saveBitInventory(player, null,
+		        //String owner, String name, String coowners, Inventory inventory,
+				//int useCost);
+		
+		
+		File saveFile;
+		if (BITConfig.getBooleanParm("SBP.InventoriesShare."
+				+ player.getWorld().getName(), true)) {
+			saveFile = new File(BIT.plugin.getDataFolder() + File.separator
+					+ "inventories", player.getName() + ".yml");
+		} else {
+			saveFile = new File(BIT.plugin.getDataFolder() + File.separator
+					+ "inventories", player.getName() + "_" + world.getName()
+					+ ".yml");
+		}
+		YamlConfiguration config = new YamlConfiguration();
+		if (BITBackpack.inventories.containsKey(player.getName())) {
+			int sizeInConfig = BITBackpack.sizeInConfig(player.getWorld(), player);
+			if (sizeInConfig > 0) {
+				Inventory inv = SpoutManager.getInventoryBuilder().construct(
+						sizeInConfig, BITBackpack.inventoryName);
+				inv.setContents(BITBackpack.inventories.get(player.getName()));
+				Integer i = 0;
+				for (i = 0; i < sizeInConfig; i++) {
+					ItemStack item = inv.getItem(i);
+					config.set(i.toString() + ".amount", item.getAmount());
+					Short durab = item.getDurability();
+					config.set(i.toString() + ".durability", durab.intValue());
+					config.set(i.toString() + ".type", item.getTypeId());
+					config.set("Size",
+							sizeInConfig);
+					try {
+						config.save(saveFile);
+					} catch (IOException e) {
+						player.sendMessage("The backpack could not be saved.");
+						e.printStackTrace();
+					}
+				}
+			}
+		} else {
+			BITBackpack.inventories.remove(player.getName());
+		}
+	}
+
+	@Override
+	public void run() {
+		if (Bukkit.getServer().getOnlinePlayers().length != 0) {
+			saveAll();
+		}
+	}
+}
